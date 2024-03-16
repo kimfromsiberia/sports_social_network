4cea4f7d4866a793f19c742e2072e68fa3a94283
Anton Kim <kimfromsiberia@gmail.com>
2024-03-16 23:06:41 +0700

    Rename app in __init__.py. Update README.md. Add requirements.txt

--- sport_social_network/__init__.py
+++ sport_social_network/__init__.py
@@ -1,15 +1,15 @@
 from flask import Flask
 from flask import render_template
 
-sport_app = Flask(__name__)
+def create_app():
+    app = Flask(__name__)
 
-@sport_app.route('/')
-def start_page():
-    return render_template('start_page.html')
+    @app.route('/')
+    def start_page():
+        return render_template('start_page.html')
 
-@sport_app.route('/registration/')
-def registration():
-    return render_template('registration_page.html')
-
-if __name__ == '__main__':
-    sport_app.run(debug=True)
+    @app.route('/registration/')
+    def registration():
+        return render_template('registration_page.html')
+    
+    return app
