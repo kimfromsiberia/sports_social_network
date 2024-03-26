f25eff6169a906441a31bbf08d07475d572b4793
Anton Kim <kimfromsiberia@gmail.com>
2024-03-27 00:16:39 +0700

    6 implement user login and registration

--- sport_social_network/__init__.py
+++ sport_social_network/__init__.py
@@ -1,7 +1,8 @@
-from flask import Flask
-from flask import render_template
+from flask import flash, Flask, redirect, request, url_for, render_template
+from flask_login import current_user, LoginManager, login_user, logout_user
+from werkzeug.security import generate_password_hash
 
-from sport_social_network.model import db
+from sport_social_network.model import db, User
 
 
 def create_app():
@@ -12,12 +13,74 @@ def create_app():
     with app.app_context():
         db.create_all()
 
-    @app.route('/')
+    login_manager = LoginManager()
+    login_manager.init_app(app)
+    login_manager.login_view = 'start_page'
+
+    
+    @login_manager.user_loader
+    def load_user(user_id):
+        return User.query.get(user_id)
+    
+    
+    @app.route('/', methods=['GET', 'POST'])
     def start_page():
+        if current_user.is_authenticated:
+            return 'Страница находится в разработке.'
+        if request.method == 'POST':
+            email = request.form['email']
+            if email:
+                user = User.query.filter(User.email == email).first()
+                if user:
+                    if user.check_password(request.form['password']):
+                        login_user(user)
+                        return 'Страница находится в разработке.'
+                    else:
+                        flash('Неверный пароль')
+                        return redirect(url_for('start_page'))
+                else:
+                    flash('Пользователя с такой почтой не существует')
+                    return redirect(url_for('start_page'))
+            else:
+                flash('Введите почту')
+                return redirect(url_for('start_page'))
         return render_template('start_page.html')
 
-    @app.route('/registration/')
+
+    @app.route('/registration/', methods=['GET', 'POST'])
     def registration():
+        if request.method == 'POST':
+            email = request.form['email']
+            if email:
+                user = User.query.filter(User.email == email).first()
+                if user:
+                    flash('Пользователь с такой почтой уже существует')
+                    return redirect(url_for('registration'))
+                else:
+                    if request.form['password'] and request.form['repeat_password']:
+                        if request.form['password'] != request.form['repeat_password']:
+                            flash('Введённые пароли не совпадают')
+                            return redirect(url_for('registration'))
+                        else:
+                            password = generate_password_hash(request.form['password'])
+                            new_user = User(email=email, password=password)
+                            db.session.add(new_user)
+                            db.session.commit()
+                            return redirect(url_for('start_page'))
+                    else:
+                        flash('Введите пароль')
+                        return redirect(url_for('registration'))
+            else:
+                flash('Введите почту')
+                return redirect(url_for('registration'))
         return render_template('registration_page.html')
+    
+
+    @app.route('/logout')
+    def logout():
+        logout_user()
+        flash('Вы разлогинились')
+        return redirect(url_for('start_page'))
+    
     
     return app
--- sport_social_network/model.py
+++ sport_social_network/model.py
@@ -1,8 +1,10 @@
+from flask_login import UserMixin
+from werkzeug.security import check_password_hash, generate_password_hash
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
-class User(db.Model):
+class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String, unique=True)
     password = db.Column(db.String)
@@ -12,6 +14,15 @@ class User(db.Model):
     male = db.Column(db.String)
     country = db.Column(db.String)
     city = db.Column(db.String)
+
+    
+    def set_password(self, password):
+        self.password = generate_password_hash(password)
+    
+
+    def check_password(self, password):
+        return check_password_hash(self.password, password)
+    
 
     def __repr__(self):
         return f'<User {self.id} {self.email}>'
